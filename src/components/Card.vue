bb6896afd52be81f034e3be1673794758c70e99e
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2020-06-05 01:42:13 -0600

    fix layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,48 +3,47 @@
         v-toolbar(color='deep-purple', dark='', flat='')
             v-toolbar-title Project Floyd
             v-spacer
-            v-tooltip(bottom='')
-            template(v-slot:activator='{ on }')
-                v-btn(href='' icon large target='_blank' v-on='on' @click="mixer = !mixer")
-                v-icon mdi-tune-vertical
-            span Mix Tracks
-            v-tooltip(bottom='')
-            template(v-slot:activator='{ on }')
-                v-btn(href='' icon large target='_blank' v-on='on')
-                v-icon mdi-download
-            span Save Offline
-            v-tooltip(bottom='')
-            template(v-slot:activator='{ on }')
-                v-btn(href='' icon large target='_blank' v-on='on')
-                v-icon mdi-help
-            span Help
-            v-tooltip(bottom='')
-            template(v-slot:activator='{ on }')
-                v-btn(href='' icon large target='_blank' v-on='on')
-                v-icon mdi-github
-            span Source
+            v-tooltip(bottom)
+                template(v-slot:activator='{ on }')
+                    v-btn(href='' icon large target='_blank' v-on='on')
+                        v-icon mdi-download
+                p Save Offline
+            v-tooltip(bottom)
+                template(v-slot:activator='{ on }')
+                    v-btn(href='' icon large target='_blank' v-on='on')
+                        v-icon mdi-help
+                p Help
+            v-tooltip(bottom)
+                template(v-slot:activator='{ on }')
+                    v-btn(href='' icon large target='_blank' v-on='on')
+                        v-icon mdi-github
+                p Source
         v-card-text
             v-btn.sync-btn(height="200px" color="amber")
-            v-icon(large) mdi-gesture-double-tap
-            span Tap to Sync
+                v-icon(large) mdi-gesture-double-tap
+                span Tap to Sync
             .vol-ctrls(v-if="mixer")
-            v-divider
-            h5 Mix Tracks
-            v-slider(v-for="i in tracks" :value="i.volume"
-                :key="i.text"
-                :label="i.mix ? i.mix : i.text"
-                prepend-icon="mdi-volume-medium"
-                append-icon="mdi-volume-high"
-                @click:prepend="i.volume--"
-                @click:append="i.volume++"
-                min="0"
-                max="11"
-                hide-details)
+                v-divider
+                h5 Mix Tracks
+                v-slider(v-for="i in tracks" :value="i.volume"
+                    :key="i.text"
+                    :label="i.mix ? i.mix : i.text"
+                    prepend-icon="mdi-volume-medium"
+                    append-icon="mdi-volume-high"
+                    @click:prepend="i.volume--"
+                    @click:append="i.volume++"
+                    min="0"
+                    max="11"
+                    hide-details)
         v-card-actions
             v-overflow-btn.track-select(v-model="selected" @change="setTrack" :items="tracks" item-text="text" item-value="text" label="Pick a track" menu-props="top" hide-details)
-            v-spacer
+            v-tooltip(top)
+                template(v-slot:activator='{ on }')
+                    v-btn(href='' icon large target='_blank' v-on='on' @click="mixer = !mixer")
+                        v-icon mdi-tune-vertical
+                p Mix Tracks
             v-btn.play-btn(@click="playing = !playing" :disabled="selected === null" fab color='light-green')
-            v-icon {{ playing ? 'mdi-pause' : 'mdi-play' }}
+                v-icon {{ playing ? 'mdi-pause' : 'mdi-play' }}
 
 </template>
 
